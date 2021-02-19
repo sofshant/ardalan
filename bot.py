35f823b48a8a86ccd36fd96086bd06a2e56bb9bc
sofshant <me@sofshant.club>
2021-02-19 09:25:08 -0500

    main bot file
    This is the file you need to run

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+import os
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+
+client = commands.Bot(command_prefix = '$') #you can change prefix here
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cmds.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cmds.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cmds.{extension}')
+    client.load_extension(f'cmds.{extension}')
+
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cmds.{filename[:-3]}')
+
+#the ping commmand
+@client.command()
+async def ping(ctx):
+        await ctx.send(f'**Pong**   {round(client.latency * 1000)} ms')
+
+@client.event
+async def on_ready():
+    print('Bot ready \n')
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client.run(TOKEN)
