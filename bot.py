13579d6426a189a51b2a847da5e9cfb7ca7dede0
sofshant <me@sofshant.club>
2021-03-26 16:28:34 -0400

    added error handler :sunglasses:

--- bot.py
+++ bot.py
@@ -5,6 +5,24 @@ from dotenv import load_dotenv
 
 client = commands.Bot(command_prefix = '$', help_command=None) #you can change prefix on the $
 
+#simple error handler
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.UserNotFound):
+        await ctx.send("couldn't find that user.")
+        return
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You arent allowed to do that!")
+        return
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('You are missing required arguments! Type "$help" for help')
+        return
+    if isinstance(error, commands.CommandNotFound):
+        return
+    if isinstance(error, commands.CommandError):
+        await ctx.send("Something went wrong.")
+        print(error)
+
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f'cmds.{extension}')
@@ -22,14 +40,14 @@ for filename in os.listdir('./cmds'):
     if filename.endswith('.py'):
         client.load_extension(f'cmds.{filename[:-3]}')
 
-#the ping commmand
+#ping command
 @client.command()
 async def ping(ctx):
-        await ctx.send(f'**Pong**   {round(client.latency * 1000)} ms')
+        await ctx.send(f'**Pong** {round(client.latency * 1000)} ms')
 
 @client.event
 async def on_ready():
-    print('Bot ready \n')
+    print('Hi stockr \n')
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
